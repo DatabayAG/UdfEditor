9f5f1cc51597ceba01627067ddc8acae49ed7bca
Marvin Beym <mBeym@databay.de>
2025-10-06 10:24:08 +0200

    Fix responsible names

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = "xudf";
 $version = "10.0.0";
 $ilias_min_version = '10.0';
 $ilias_max_version = '10.999';
-$responsible = "mbeym | mjansen";
+$responsible = "Marvin Beym | Michael Jansen";
 $responsible_mail = "mbeym@databay.de mjansen@databay.de";
